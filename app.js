bb06f5d7a5e48a5daf02d7dfd672dabea7e96bc0
async <harshu8545@gmail.com>
2019-09-15 14:06:38 +0530

    final app update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const expressSession = require('express-session');
 const passport = require('passport');
 const indexRouter = require('./routes/index');
 const userModel = require('./routes/users');
+const mongoose = require('mongoose');
 const app = express();
 
 
