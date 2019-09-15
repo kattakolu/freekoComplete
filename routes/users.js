f77ea8f886ae8d04216f0b786512befb78bd9449
async <harshu8545@gmail.com>
2019-09-15 14:03:09 +0530

    final app update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-mongoose.connect('mongodb+srv://admin:admin00007@cluster0-9zemc.mongodb.net/test?retryWrites=true&w=majority/freekoLab');
+mongoose.connect('mongodb+srv://admin:admin00007@cluster0-9zemc.mongodb.net/test?retryWrites=true&w=majority/freekoLab')
+  .then(function(){
+    console.log('connected to db');
+  })
 
 const userSchema = mongoose.Schema({
   name: String,
